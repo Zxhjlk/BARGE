8ba21b107b89db0f6b9e7f3cfe1959847e0436e6
dinobenj <dino.benj@gmail.com>
2023-10-19 16:11:55 -0400

    :wq

--- App/main.py
+++ App/main.py
@@ -1,9 +1,19 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QMainWindow
 import sys
+
+
+class MainWindow(QMainWindow):
+      def __init__(self):
+            super(MainWindow, self).__init__()
+            self.setWindowTitle("Barge Kanban")            
+            layout = QVBoxLayout()
+            widget = QWidget()
+            widget.setLayout(layout)
+            self.setCentralWidget(widget)
 
 if __name__ == "__main__":
       app = QApplication(sys.argv)
-      window = QWidget()
+      window = MainWindow()
       window.show()
       app.exec()
       #Main launch of window
